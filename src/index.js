00f43b6abac982df1cd10aea299291fe1e1cf15c
Eyad muhammad <eyadmuhammad90@gmail.com>
2023-09-15 23:18:02 +0300

    fixed bug v-1.2

--- src/index.js
+++ src/index.js
@@ -3,19 +3,19 @@ const details = [
     title: `Bookmark in one click`,
     text: `Organize your bookmarks however you like. Our simple drag-and-drop interface 
   gives you complete control over how you manage your favourite sites.`,
-    img: `../images/illustration-features-tab-1.svg`,
+    img: `./images/illustration-features-tab-1.svg`,
   },
   {
     title: `Intelligent search`,
     text: `Our powerful search feature will help you find saved sites in no time at all. 
     No need to trawl through all of your bookmarks.`,
-    img: `../images/illustration-features-tab-2.svg`,
+    img: `./images/illustration-features-tab-2.svg`,
   },
   {
     title: `Share your bookmarks`,
     text: `Easily share your bookmarks and collections with others. Create a shareable 
     link that you can send at the click of a button.`,
-    img: `../images/illustration-features-tab-3.svg`,
+    img: `./images/illustration-features-tab-3.svg`,
   },
 ];
 const email_form = document.querySelector("input");
